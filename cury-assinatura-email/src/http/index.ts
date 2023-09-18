509a82a95f3c0008c0dd0b44e2994af2026b4612
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2023-09-18 12:59:31 -0300

    fix build

---
+++ cury-assinatura-email/src/http/index.ts
@@ -0,0 +1,19 @@
+const api_url = process.env.API_URL || "http://localhost:3000/api";
+
+async function request(url: any, options: object) {
+  const req = await fetch(url, options);
+  const resp = await req.json();
+
+  return resp;
+}
+
+// Validar codigo de acesso
+export function validarAcesso(data: any) {
+  const options = {
+    method: "POST",
+    "Content-Type": "application/json; charset=utf-8",
+    body: JSON.stringify(data),
+  };
+
+  return request(`${api_url}/codigo-acesso`, options);
+}
